cc3a95dce337656f052064894769890cd5e7e69f
Noah Buchanan <noah6544@gmail.com>
2023-11-22 16:58:38 -0500

    testing if this is right branch

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import time
 from datetime import datetime as dt
 
 #Linux host path
-host_path = r"/etc/hosts"
+host_path = r"C:/Windows/System32/drivers/etc/hosts"
 #Local host IP address 
 redirect = "127.0.0.1"
 
@@ -17,21 +17,21 @@ active = True
 while active:
 
     #Time period the block is active
-    if dt(dt.now().year, dt.now().month, dt.now().day, 23) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 24):
+    print(dt(dt.now().year, dt.now().month, dt.now().day, 0) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 23))
+    if dt(dt.now().year, dt.now().month, dt.now().day, 0) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 23):
 
         print("No distractions...try again later")
         with open(host_path, 'r+') as file:
             #Contents of the file
             content = file.read()
             print(content)
-
-        for website in websites_blocked:
-            #If the website is already there dont do anything
-            if website in content:
-                pass
-            else:
-                #Map the hostname to local host IP
-                file.write(redirect + " " + website  + " \n")                
+            for website in websites_blocked:
+                #If the website is already there dont do anything
+                if website in content:
+                    pass
+                else:
+                    #Map the hostname to local host IP
+                    file.write(redirect + " " + website  + " \n")                
     else:
         with open(host_path, 'r+') as file:
             content = file.readlines()
